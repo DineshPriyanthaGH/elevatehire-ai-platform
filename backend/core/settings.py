d0f0f598879a71488ab7926ed838ec5615374d26
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-07-25 00:31:47 +0530

    database connection

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'elevatehire_db',
+        'USER': 'postgres',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
